9d5b36789908cefd5e6cccdbfaac3ceb26d80353
rdyja <rdyja@icis.pcz.pl>
2015-05-09 14:01:22 +0200

    Preliminary work on boundary conditions of the 4th type. I have added new class ContactBounds.

---
+++ contact_bounds.hpp
@@ -0,0 +1,68 @@
+/*
+ * contact_bounds.hpp
+ *
+ *  Created on: 07-05-2015
+ *      Author: rdyja
+ */
+
+#ifndef CONTACT_BOUNDS_HPP_
+#define CONTACT_BOUNDS_HPP_
+
+#include <petsc.h>  // for PETSc objects and types
+
+#include <map>  // for std::map
+#include <vector>  // for std::vector
+
+#include "Common/TALYException.h"  // for throwing exception
+#include "DataStructure/zeroarray.h" //for ZEROARRAY
+#include "Grid/grid_types/grid.h"  // for GRID class
+#include "FEM/periodic_bounds.h" // for PeriodicMap
+
+namespace TALYFEMLIB {
+
+/*
+ * ContactBounds is similar to PeriodicBounds class. The main difference
+ * is not calculating periodic partners, but loading them from file. The other
+ * difference is that it doesn't store data about periodic boundaries, only
+ * about periodic nodes.
+*/
+class ContactBounds {
+public:
+	// Default constructor
+	ContactBounds(): p_grid_(nullptr),  is_periodic_(false) { }
+	virtual ~ContactBounds() { }
+
+	  inline bool is_periodic() { return is_periodic_; }
+	  inline bool IsNodePeriodic(int index) {
+	    return is_node_periodic_.get(index);
+	  }
+	  inline int GetPeriodicPartner(int index) {
+	    return pbc_partners_[index];
+	  }
+	  inline int GetPeriodicSolPartner(int index) {
+	    return pbc_sol_partners_[index];
+	  }
+	  inline const PeriodicMap* pbc_partners() const {
+	    return &pbc_partners_;
+	  }
+	  inline const PeriodicMap* pbc_sol_partners() const {
+	    return &pbc_sol_partners_;
+	  }
+
+	  inline GRID* Grid() { return p_grid_; }
+
+
+	void LoadContactBounds(GRID *pg,
+			const std::vector<PetscInt>& oldID, const std::vector<PetscInt>& newID);
+
+private:
+	GRID* p_grid_;
+	bool is_periodic_;
+	ZEROARRAY<bool> is_node_periodic_;
+	PeriodicMap pbc_partners_;
+	PeriodicMap pbc_sol_partners_;
+};
+
+}
+
+#endif /* CONTACT_BOUNDS_HPP_ */
