463ddbe912f991dbee0eefe95bb0f4e9882f43e9
Robert Dyja <robert.dyja@icis.pcz.pl>
2015-11-16 13:07:53 +0100

    Solved some problems with ContactBounds::ImportContactBounds()

--- contact_bounds.cpp
+++ contact_bounds.cpp
@@ -37,49 +37,56 @@ void ContactBounds::LoadContactBounds(GRID *pg,
 
 void ContactBounds::ImportContactBounds(GRID *pg) {
 
+	  if (pg->get_node_periodics().size() > 0) {
+		  has_periodic_ = true;
+//		  PrintInfo("TransferContactBounds");
+	  } else {
+		  has_periodic_ = false;
+		  return;
+	  }
+
 	  p_grid_ = pg;
-
-//	  PrintInfo("TransferContactBounds");
-
 
 	  const map<int,int>& grid_periodics = pg->get_node_periodics();
 
 	  // set the partners for the boundaries
-	  is_node_periodic_.redim(this->p_grid_->n_nodes());
+	  is_node_periodic_.redim(p_grid_->n_nodes());
 	  is_node_periodic_.fill(false);
+
+//	  for (int i = 0; i < pg->n_nodes(); i++) {
+//		  PetscSynchronizedPrintf(PETSC_COMM_WORLD,
+//		  				  "%d) physical_map=%d  solution_map=%d\n",
+//						  i, pg->physical_map(i), pg->solution_map(i));
+//	  }
+//	  PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT);
 
 	  for (map<int,int>::const_iterator it = grid_periodics.begin();
 			  it != grid_periodics.end(); ++it) {
 		  PetscSynchronizedPrintf(PETSC_COMM_WORLD,
 				  "grid_periodics: first=%d  second=%d\n", it->first, it->second);
-		  if (p_grid_->parallel_type_ == kWithDomainDecomp) {
-			  PetscSynchronizedPrintf(PETSC_COMM_WORLD,
-					  "grid_periodics: solution_map(first)=%d solution_map(second)=%d\n",
-					  p_grid_->solution_map(it->first), p_grid_->solution_map(it->second));
-		  }
-		  PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT);
 	  }
+	  PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT);
 
 	  for (map<int,int>::const_iterator it = grid_periodics.begin();
 			  it != grid_periodics.end(); ++it) {
 
 		if (p_grid_->parallel_type_ == kWithDomainDecomp) {
-//			throw TALYException() << "Domain Decomposition unsupported in LoadPeriodicBounds";
-			is_node_periodic_.set(it->first, true);
-			pbc_partners_[it->first] = it->second;
-			pbc_sol_partners_[it->first] = p_grid_->solution_map(it->second);
-		} else {
-			is_node_periodic_.set(it->first, true);
 			pbc_partners_[it->first] = it->second;
 			pbc_sol_partners_[it->first] = it->second;
+			// is_node_periodic must be handled in special way
+		} else {
+			pbc_partners_[it->first] = it->second;
+			pbc_sol_partners_[it->first] = it->second;
+			is_node_periodic_.set(it->first, true);
 		}
 
 	  }
 
-	  if (pg->get_node_periodics().size() > 0) {
-		  has_periodic_ = true;
-	  } else {
-		  has_periodic_ = false;
+	  if (p_grid_->parallel_type_ == kWithDomainDecomp) {
+		  for (int i = 0; i < pg->n_nodes(); i++) {
+			  if (grid_periodics.find(p_grid_->solution_map(i)) != grid_periodics.end())
+				  is_node_periodic_.set(i, true);
+		  }
 	  }
 
 }
